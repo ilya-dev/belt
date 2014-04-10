8ad68fec7ffd6b3ac88f7e6f532b675f2dfba4cc
Ilya S <public.ilya.s@gmail.com>
2014-04-10 21:10:20 +0700

    create Belt\Funcs

---
+++ spec/Belt/FuncsSpec.php
@@ -0,0 +1,16 @@
+<?php namespace spec\Belt;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FuncsSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Belt\Funcs');
+    }
+
+
+
+}
+
---
+++ src/Belt/Funcs.php
@@ -0,0 +1,8 @@
+<?php namespace Belt;
+
+class Funcs extends Toolset {
+
+
+
+}
+
