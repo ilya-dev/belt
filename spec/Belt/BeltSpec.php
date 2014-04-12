a8b7c98c25828e40b59452ecd73e798158fb804c
Ilya S <public.ilya.s@gmail.com>
2014-04-12 20:30:18 +0700

    Add isLoaded & load; write tests

--- spec/Belt/BeltSpec.php
+++ spec/Belt/BeltSpec.php
@@ -10,11 +10,29 @@ class BeltSpec extends ObjectBehavior {
         $this->shouldHaveType('Belt\Belt');
     }
 
-    function it_returns_the_list_of_the_modules()
+    function it_returns_all_modules()
     {
         $this->getModules()->shouldBeArray();
 
         $this->getModules()->shouldHaveCount(5);
+    }
+
+    function it_loads_a_module()
+    {
+        $this->load('Belt\Collections')->shouldHaveType('Belt\Collections');
+
+        $this->shouldThrow('UnexpectedValueException')->duringLoad('fsdwfwgwa');
+
+        $this->load('foo', new \stdClass)->shouldHaveType('stdClass');
+    }
+
+    function it_determines_whether_the_module_was_loaded()
+    {
+        $this->isLoaded('foo')->shouldBe(false);
+
+        $this->load('foo', new \stdClass);
+
+        $this->isLoaded('foo')->shouldBe(true);
     }
 
 }
--- src/Belt/Belt.php
+++ src/Belt/Belt.php
@@ -23,6 +23,29 @@ class Belt {
     protected $instances = [];
 
     /**
+     * Load a module
+     *
+     * @param  string $module
+     * @param  mixed|null $instance
+     * @throws \UnexpectedValueException
+     * @return mixed
+     */
+    public function load($module, $instance = null)
+    {
+        if ( ! \is_null($instance))
+        {
+            return $this->instances[$module] = $instance;
+        }
+
+        if (\in_array($module, $this->modules))
+        {
+            return $this->instances[$module] = new $module;
+        }
+
+        throw new \UnexpectedValueException("Module {$module} does not exist");
+    }
+
+    /**
      * Get the list of the modules
      *
      * @return array
@@ -30,6 +53,17 @@ class Belt {
     public function getModules()
     {
         return $this->modules;
+    }
+
+    /**
+     * Determine whether the module was loaded
+     *
+     * @param  string  $module
+     * @return boolean
+     */
+    public function isLoaded($module)
+    {
+        return \array_key_exists($module, $this->instances);
     }
 
     /**
