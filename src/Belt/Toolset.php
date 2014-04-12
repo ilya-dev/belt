4b1daa0d46f1e14275a45d12db269d5297494a4c
Ilya S <public.ilya.s@gmail.com>
2014-04-12 23:40:17 +0700

    Update Toolset, add some syntactic sugar

--- src/Belt/Toolset.php
+++ src/Belt/Toolset.php
@@ -13,5 +13,19 @@ abstract class Toolset {
         return new Chain($this, $value);
     }
 
+    /**
+     * Handle dynamic static calls
+     *
+     * @param  string $method
+     * @param  array  $arguments
+     * @return mixed
+     */
+    public static function __callStatic($method, array $arguments = array())
+    {
+        $method = \str_replace('_', '', $method);
+
+        return \call_user_func_array([new static, $method], $arguments);
+    }
+
 }
 
