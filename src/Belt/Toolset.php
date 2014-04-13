7181f17200328ddbc9a921a637c9c3240b1ba0a3
Ilya S <public.ilya.s@gmail.com>
2014-04-13 13:48:09 +0700

    Remove the chaining functionality

--- src/Belt/Toolset.php
+++ src/Belt/Toolset.php
@@ -1,31 +1,4 @@
 <?php namespace Belt;
 
-abstract class Toolset {
-
-    /**
-     * Start a new "chain"
-     *
-     * @param  mixed $value
-     * @return \Belt\Chain
-     */
-    public function chain($value = null)
-    {
-        return new Chain($this, $value);
-    }
-
-    /**
-     * Handle dynamic static calls
-     *
-     * @param  string $method
-     * @param  array  $arguments
-     * @return mixed
-     */
-    public static function __callStatic($method, array $arguments = array())
-    {
-        $method = \str_replace('_', '', $method);
-
-        return \call_user_func_array([new static, $method], $arguments);
-    }
-
-}
+abstract class Toolset {}
 
